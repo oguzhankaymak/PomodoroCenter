da7006d487a7f5205600e440cfad63f2e4922d84
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2022-09-30 19:28:49 +0300

    Add equatable extension for tests

--- PomodoroCenter/Models/TimeByDay.swift
+++ PomodoroCenter/Models/TimeByDay.swift
@@ -3,5 +3,12 @@ import Foundation
 struct TimeByDay {
     var dayOfWeek: String
     var minutes: Double
-   
+}
+
+
+extension TimeByDay: Equatable {
+    static func ==(lhs: TimeByDay, rhs: TimeByDay) -> Bool {
+        return lhs.dayOfWeek == rhs.dayOfWeek
+        && lhs.minutes == rhs.minutes
+    }
 }
--- PomodoroCenter/Models/TimeByMonth.swift
+++ PomodoroCenter/Models/TimeByMonth.swift
@@ -3,5 +3,12 @@ import Foundation
 struct TimeByMonth {
     var monthOfYear: String
     var hours: Double
-   
+}
+
+
+extension TimeByMonth: Equatable {
+    static func ==(lhs: TimeByMonth, rhs: TimeByMonth) -> Bool {
+        return lhs.monthOfYear == rhs.monthOfYear
+        && lhs.hours == rhs.hours
+    }
 }
